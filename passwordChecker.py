b2d6b98a6fe991987d45a9fc3005b423b30e77f3
Chav Sumalinog <chaviotics@gmail.com>
2021-02-12 10:08:30 +0800

    Added functions get_password_leaks_count & main and tested the file.

--- passwordChecker.py
+++ passwordChecker.py
@@ -1,5 +1,6 @@
 import requests # allows us to have a browser # we use the api of pwnedpasswords.
 import hashlib # lets us import the SHA1
+import sys
 
 # url = 'https://api.pwnedpasswords.com/range/' + '20177' # uses SHA1 Algo from 20177DAFA4C488828B70993C896D5ECA7A38C9C2
 # res = requests.get(url)
@@ -15,10 +16,34 @@ def requests_api_data(query_char):
         raise RuntimeError(f"Error fetching {res.status_code}, check api and try again.") 
     return res
 
+def get_password_leaks_count(hashes, hash_to_check): # hash_to_check is our password tail to be checked
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    # print(hashes)
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password): # checks the password if it exists in API response
-    # print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = requests_api_data(first5_char)
+    # print(sha1password)
+    # print(first5_char, tail)
+    # print(response)
+    return get_password_leaks_count(response, tail)
 
 # requests_api_data('123')
-pwned_api_check('123')
+# pwned_api_check('123')
+
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(f'{password} was found {count} times. You should change your password.')
+        else:
+            print(f"{password} was NOT found. Good password! Carry on!")
+    print("All done!")
+
+main(sys.argv[1:])
